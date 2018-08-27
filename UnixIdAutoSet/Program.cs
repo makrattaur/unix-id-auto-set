869e90f8074f7b2344822da48709c94243ef7420
Michael Tran <mike_tran@live.com>
2018-08-26 22:34:38 -0400

    Add support for Unix user private groups

--- UnixIdAutoSet/Program.cs
+++ UnixIdAutoSet/Program.cs
@@ -12,6 +12,7 @@ namespace UnixIdAutoSet
     {
         static DateTime kUnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         static string kUnixObjectsGroup = "CN=Unix Objects,OU=Groups";
+        static string kUserPrivateGroupContainer = "OU=Unix User Private Groups,OU=Groups";
 
         static void Main(string[] args)
         {
@@ -20,9 +21,10 @@ namespace UnixIdAutoSet
                 conn.Open();
                 CreateTables(conn);
 
+                string directorySuffix;
                 using (DirectoryEntry ldapConn = new DirectoryEntry())
                 {
-                    string directorySuffix = (string)ldapConn.Properties["distinguishedName"].Value;
+                    directorySuffix = (string)ldapConn.Properties["distinguishedName"].Value;
 
                     string userFilter = string.Format(
                         "(&(objectClass=user)(memberOf={0},{1})(!uidNumber=*))",
@@ -39,6 +41,7 @@ namespace UnixIdAutoSet
                     UpdateObjects(conn, ldapConn, groupFilter, "gidNumber", "NextGroupId");
                 }
 
+                UpdateUserPrivateGroups(conn, directorySuffix);
             }
 
             int x = 42;
@@ -68,6 +71,20 @@ namespace UnixIdAutoSet
 
                     Console.WriteLine("Set id {0} for entry {1} ({2})", id, entry.Guid, dn);
                 }
+            }
+        }
+
+        static void UpdateUserPrivateGroups(SQLiteConnection conn, string directorySuffix)
+        {
+            string ldapPath = string.Format(
+                "LDAP://{0},{1}",
+                kUserPrivateGroupContainer,
+                directorySuffix
+            );
+
+            using (DirectoryEntry ldapConn = new DirectoryEntry(ldapPath))
+            {
+                UpdateObjects(conn, ldapConn, "(&(objectClass=group)(!gidNumber=*))", "gidNumber", "NextGroupId");
             }
         }
 
